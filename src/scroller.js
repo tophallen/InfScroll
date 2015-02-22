1749686930a7d8acdff06cd030ec471a2a09e27d
tophallen <tophallen@me.com>
2015-02-21 19:52:30 -0800

    updated references from working title
    `Scroller` is too generic, completed rename to `InfScroll`

--- src/scroller.js
+++ src/scroller.js
@@ -1,18 +1,14 @@
 /*
 The MIT License (MIT)
-
 Copyright (c) 2015 Chris Allen
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -286,24 +282,24 @@ THE SOFTWARE.
       buildWindowOnScrollWatch: function(el, callback) {
         if (!el.onscroll || el.onscroll.name != 'infinityCallback') {
           var infinityCallback = function infinityCallback() {
-            Scroller._windowOnScrollCallbacks.forEach(function(item) {
+            InfScroll._windowOnScrollCallbacks.forEach(function(item) {
               item();
             });
           };
           el.onscroll = infinityCallback;
         }
-        Scroller._windowOnScrollCallbacks.push(callback);
+        InfScroll._windowOnScrollCallbacks.push(callback);
       },
       buildWindowOnResizeWatch: function(callback) {
         if (!window.onresize || window.onresize.name != 'infinityCallback') {
           var infinityCallback = function infinityCallback() {
-            Scroller._windowOnResizeCallbacks.forEach(function(item) {
+            InfScroll._windowOnResizeCallbacks.forEach(function(item) {
               item();
             });
           };
           window.onresize = infinityCallback;
         }
-        Scroller._windowOnResizeCallbacks.push(callback);
+        InfScroll._windowOnResizeCallbacks.push(callback);
       },
       buildAjaxRequest: function(config) {
         var xhr = config.defaultAjaxConfig;
